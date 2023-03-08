8356eef9f7cb1646abc2575f56076e52edc71f01
cjboswell98 <116133429+cjboswell98@users.noreply.github.com>
2023-03-08 18:47:13 -0500

    f

---
+++ todo-app/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  todos: Todo[] = [
+    { task: 'Learn Angular', completed: true },
+    { task: 'Build a todo app', completed: false },
+    { task: 'Deploy to production', completed: false }
+  ];
+  
+  addTask(task: string) {
+    const newTodo: Todo = { task, completed: false };
+    this.todos.push(newTodo);
+  }
+  
+  completeTask(todo: Todo) {
+    todo.completed = true;
+  }
+  
+  removeTask(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+}
